0b9149be5cc815a18c7325069a007befecd52215
AryanChd <chaudharyaryancr7@gmail.com>
2025-07-29 10:51:32 +0545

    fix: resolve 'next' undefined error in admin middleware

--- backend/src/middleware/isAdmin.js
+++ backend/src/middleware/isAdmin.js
@@ -1,25 +1,17 @@
-const isAdmin = (req,res)=> { 
+const isAdmin = (req, res, next) => {
+  try {
+    const user = req.user;
 
-try {
-
-        const user = req.user 
-
-    if(user.role !== 'ADMIN'){
-        throw new Error("User is not a admin !")
-
+    if (user.role !== "ADMIN") {
+      throw new Error("User is not a admin !");
     }
-    next()
-
-} catch (error) {
-
+    next();
+  } catch (error) {
     res.status(403).json({
-        message: "User id not Admin !",
-        error:error.message
+      message: "User id not Admin !",
+      error: error.message,
+    });
+  }
+};
 
-    })
-    
-}
-
-}
-
-export {isAdmin}    
+export { isAdmin };
