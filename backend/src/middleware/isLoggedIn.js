f6c6900224ade7b8296348f69e1247eae33478c0
AryanChd <chaudharyaryancr7@gmail.com>
2025-07-30 10:55:22 +0545

    refactor: enhance authentication middleware

--- backend/src/middleware/isLoggedIn.js
+++ backend/src/middleware/isLoggedIn.js
@@ -1,33 +1,22 @@
-
-import { verifyToken } from "../helper/token.js"
+import { verifyToken } from "../helper/token.js";
 
 const isLoggedIn = (req, res, next) => {
+  try {
+    const token = req.cookies.authToken;
 
-    try {
-
-        const token = req.cookies.authToken
-
-        if (!token) { throw new Error(" User not authorized !") }
-
-        console.log(token)
-
-        const decoded = verifyToken(token)
-        console.log(decoded)
-
-
-
-        req.user = decoded
-        next();
-
-
-
-    } catch (error) {
-
-        console.log(error.message)
-
-        res.send(error.message)
+    if (!token) {
+      throw new Error(" User not authorized !");
     }
-}
 
-export { isLoggedIn }
+    const decoded = verifyToken(token);
 
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.log(error.message);
+
+    res.send(error.message);
+  }
+};
+
+export { isLoggedIn };
