cd747fea278fe69d7100b701f24dcd6e74939c0e
AryanChd <chaudharyaryancr7@gmail.com>
2025-08-03 12:39:18 +0545

    feat: update updated getorderbyid getorderbyuserid updateorderstatus updatepaymentstatus  routes configuration

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -1,9 +1,20 @@
 import express from "express";
 import { isLoggedIn } from "../middleware/isLoggedin.js";
-import { createOrder } from "../controllers/orderController.js";
+import {isAdmin} from "../middleware/isAdmin.js"
+import {
+  createOrder,
+  getOrderById,
+  getOrderByUserId,
+  updateOrderStatus,
+  updatePaymentStatus,
+} from "../controllers/orderController.js";
 
 const router = express.Router();
 
 router.post("/createOrder", isLoggedIn, createOrder);
+router.get("/getOrder/:id", isLoggedIn, getOrderById);
+router.get("/getOrderByUser", isLoggedIn, getOrderByUserId);
+router.post("/updateOrderStatus/:id", isLoggedIn, isAdmin, updateOrderStatus);
+router.post("/updatePaymentStatus/:id", isLoggedIn, isAdmin, updatePaymentStatus);
 
 export default router;
