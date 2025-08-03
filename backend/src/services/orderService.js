91e2fca88f0ecb96bb3808b4f5536c47283e378a
AryanChd <chaudharyaryancr7@gmail.com>
2025-08-03 12:40:01 +0545

    feat: update order getorderbyid getorderbyuserid updateorderstatus updatepaymentstatus  service logic

--- backend/src/services/orderService.js
+++ backend/src/services/orderService.js
@@ -5,4 +5,38 @@ const createOrder = async (order) => {
   return data;
 };
 
-export default { createOrder };
+const getOrderById = (id) => {
+  return Order.findById(id);
+};
+
+const getOrderByUserId = (userId) => {
+  return Order.find({ user: userId });
+};
+
+const updateOrderStatus = async (id, status) => {
+  await Order.findByIdAndUpdate(
+    id,
+    {
+      orderStatus: status,
+    },
+    { new: true }
+  );
+};
+
+const updatePaymentStatus = async (id, status) => {
+  await Order.findByIdAndUpdate(
+    id,
+    {
+      paymentStatus: status,
+    },
+    { new: true }
+  );
+};
+
+export default {
+  createOrder,
+  getOrderById,
+  getOrderByUserId,
+  updateOrderStatus,
+  updatePaymentStatus,
+};
