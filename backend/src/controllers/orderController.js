285ee599ee16a94f227b904ce3719734dac66807
AryanChd <chaudharyaryancr7@gmail.com>
2025-08-04 10:45:33 +0545

    feat: integrate Khalti payment method in order controller

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -1,4 +1,5 @@
 import orderService from "../services/orderService.js";
+import axios from "axios";
 
 const createOrder = async (req, res) => {
   try {
@@ -6,6 +7,34 @@ const createOrder = async (req, res) => {
     console.log(userId.id);
     const order = req.body;
     order.user = userId;
+
+    if (order.paymentMethod == "khalti") {
+      console.log(order);
+
+      const totalAmount = order.totalAmount;
+
+      const options = {
+        return_url: "http://localhost:5173/dashboard",
+        website_url: "http://localhost:5173",
+        amount: totalAmount * 100,
+        purchase_order_id: Date.now(),
+        purchase_order_name: `order- ${Date.now()}`,
+      };
+
+      const result = await axios.post(
+        "https://dev.khalti.com/api/v2/epayment/initiate/",
+        options,
+        {
+          headers: {
+            Authorization: `key ${process.env.KHALTI_SECRET_KEY}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(result.data);
+      return res.status(200).send(result.data);
+    }
+
     const data = await orderService.createOrder(order);
     console.log(data);
     res.status(200).json({ data, message: "Order created sucessfully" });
